4053f08e6d2b44b60ac8e98c0b82210a92a93d84
Jonathan Tavarez <jtavarez@tjss.us>
2024-04-30 11:21:32 -0400

    303 Practice SBA 3 Substraction

---
+++ src/practice/subtraction/Subtraction.java
@@ -0,0 +1,26 @@
+package practice.subtraction;
+
+public class Subtraction {
+
+  public static void main(String[] args) {
+    System.out.println("303 Practice SBA 1 Division\n");
+    substractionString("824");
+  }
+
+  private static void substractionString(String str) {
+    System.out.println("*********divisionString*********");
+
+    char[] digitsArray = str.toCharArray();
+    int result = Character.getNumericValue(digitsArray[0]);
+
+    if (str.length() >= 2) {
+      for (int i = 1; i < digitsArray.length; i++) {
+        int currentDigit = Character.getNumericValue(digitsArray[i]);
+        System.out.print(result + " - " + currentDigit);
+        result = result - currentDigit;
+        System.out.println(" = " + result);
+      }
+    }
+    System.out.println("Final Result: " + result);
+  }
+}
