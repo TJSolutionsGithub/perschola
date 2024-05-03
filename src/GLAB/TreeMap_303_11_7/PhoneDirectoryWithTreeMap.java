e0067fa213516860976051ab87adc9208047b4b3
Jonathan Tavarez <jtavarez@tjss.us>
2024-05-03 16:14:57 -0400

    Guided LAB - 303.11.7 - Phone Directory Using TreeMap

---
+++ src/GLAB/TreeMap_303_11_7/PhoneDirectoryWithTreeMap.java
@@ -0,0 +1,49 @@
+package GLAB.TreeMap_303_11_7;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PhoneDirectoryWithTreeMap {
+  /**
+   * The TreeMap that will store the data.  Both key and value are
+   * of type String.  The key represents a name and the value represents
+   * the associated phone number.
+   */
+  private TreeMap<String, String> data;
+
+  /**
+   * Constructor creates an initially empty directory.
+   */
+  public PhoneDirectoryWithTreeMap(){
+    this.data = new TreeMap<>();
+  }
+
+  /**
+   * Finds the phone number, if any, for a given name.
+   * @return The phone number associated with the name; if the name does
+   * not occur in the phone directory, then the return value is null.
+   */
+  public String getNumber(String name){
+    return this.data.get(name);
+  }
+
+  /**
+   * Associates a given name with a given phone number.  If the name
+   * already exists in the phone directory, then the new number replaces
+   * the old one.  Otherwise, a new name/number pair is added.  The
+   * name and number should both be non-null.  An IllegalArgumentException
+   * is thrown if this is not the case.    */
+public void putNumber(String name, String number){
+  //validate parameters
+  if (name == null || number == null) {
+    throw new IllegalArgumentException("Name and number cannot be null");
+  }
+  this.data.put(name, number);
+}
+
+  public void print() {
+  for (Map.Entry<String, String> entry : data.entrySet()) {
+    System.out.println(entry.getKey() + ": " + entry.getValue());
+  }
+  }
+}
